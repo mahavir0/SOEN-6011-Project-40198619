dd2e76f2ce3e90c875677e0c3ecb3af6f5ef3a6e
mahavir0 <170010116034@adit.ac.in>
2022-07-18 12:09:08 -0400

    Beta function for integers

--- src/main/Function6.java
+++ src/main/Function6.java
@@ -3,6 +3,8 @@
  */
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author Mahavir
  *
@@ -20,8 +22,56 @@ public class Function6 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub\
+		double x,y;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Beta Function Calculator B(X, Y)");
+		System.out.println("Enter X (X>0)(Only int numbers) : ");
+		x = sc.nextDouble();
+		System.out.println("Enter Y (Y>0)(Only int numbers) : ");
+		y = sc.nextDouble();
+		System.out.println("B(X, Y) : "+betaFucntion(x, y));
+		sc.close();
 	}
+	
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	static double betaFucntion(double x, double y) {
+		return gammaFunction(x) * gammaFunction(y) / gammaFunction(x+y);
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
+	static double gammaFunction(double n) {
+		return factorial(n-1);
+	}
+	/**
+	 * 
+	 * @param n
+	 * @param a
+	 * @return
+	 */
+	static double factorialTailRecursive(double n,double a) {
+		if(n==0)
+			return a;
+		return factorialTailRecursive(n-1,n*a);
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @return
+	 */
+	static double factorial(double n) {
+		return factorialTailRecursive(n, 1);
+	}
+	
 
 }

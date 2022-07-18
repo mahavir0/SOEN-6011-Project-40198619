66a800cb485b94c486b8512c557fe75549eb822b
mahavir0 <170010116034@adit.ac.in>
2022-07-18 09:35:32 -0400

    Project initialized

---
+++ src/main/Function6.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Mahavir
+ *
+ */
+public class Function6 {
+
+	/**
+	 * 
+	 */
+	public Function6() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

5169bee38bd5400a6a39efe29585eb254636fade
mahavir0 <170010116034@adit.ac.in>
2022-07-31 00:32:27 -0400

    Added Problem 1 description

--- src/main/Function6.java
+++ src/main/Function6.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package main;
 
 import java.util.Scanner;
@@ -11,9 +12,9 @@ import java.util.Scanner;
  */
 public class Function6 {
 
-	public static final double PI = 3.141592653589793;
-	public static final double e = 2.718281828459045;
-	
+	private static final double PI = 3.141592653589793;
+	private static final double e = 2.718281828459045;
+
 	/**
 	 * driver function to take inputs from the user and returns the beta value.
 	 * 
@@ -22,180 +23,187 @@ public class Function6 {
 	public static void main(String[] args) {
 		Function6 betafucntionobejct = new Function6();
 		// TODO Auto-generated method stub\
-		while(true) {
+		while (true) {
 			Scanner sc = new Scanner(System.in);
-			String f="";
-			double x,y;
+			String f = "";
+			double x, y;
 			System.out.println("Beta Function Calculator B(X, Y)");
 			try {
 				System.out.println("Enter X (X>0) : ");
 				x = sc.nextDouble();
 				System.out.println("Enter Y (Y>0) : ");
 				y = sc.nextDouble();
-				if(x<=0 || y<=0) {
+				if (x <= 0 || y <= 0) {
 					System.out.println("Error ! Please Enter the Positive Numbers Only !!");
 					continue;
 				}
-				System.out.println("B(X, Y) : "+betafucntionobejct.betaFucntion(x, y));
+				System.out.println("B(X, Y) : " + betafucntionobejct.betaFucntion(x, y));
 				System.out.println("Press 'Y' to continue\nPress 0 to exit .............");
 				f = sc.next();
-				if(f.equals("0")) {
+				if (f.equals("0")) {
 					sc.close();
 					break;
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				System.out.println("Error ! Please Enter the valid Inputs !!");
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Returns the beta value for the x and y
 	 * 
 	 * @param x
 	 * @param y
-	 * @return
+	 * @return B(x,y)
 	 */
 	public double betaFucntion(double x, double y) {
-		return gammaFunction(x) * gammaFunction(y) / gammaFunction(x+y);
+		return gammaFunction(x) * gammaFunction(y) / gammaFunction(x + y);
 	}
-	
+
 	/**
-	 * Return the gamma value for the Real positive numbers(R+) using the stirling approximation
+	 * Return the gamma value for the Real positive numbers(R+) using the stirling
+	 * approximation
 	 * 
 	 * @param n
-	 * @return
+	 * @return gamma value for the decimal numbers using the stirling's approximation
 	 */
 	public double decimalGammaFunction(double n) {
 		n = n - 1;
-		double firstpart = 2*PI*n;
-		double secondpart = (n/e); 
-		double result = power(firstpart,0.5)*power(secondpart,n); 
+		double firstpart = 2 * PI * n;
+		double secondpart = (n / e);
+		double result = power(firstpart, 0.5) * power(secondpart, n);
 		return result;
 	}
-	
+
 	/**
 	 * Return the gamma value for the integers only by using Factorial Method
 	 * 
 	 *
 	 * @param n
-	 * @return
+	 * @return gamma value for the number, gamma of n = (n-1)!
 	 */
 	public double gammaFunction(double n) {
-		if(checkWholeNumber(n))
-			return factorial(n-1);
+		if (checkWholeNumber(n)) {
+			return factorial(n - 1);
+		}
 		return decimalGammaFunction(n);
 	}
-	
+
 	/**
-	 * Return the power of n to a 
+	 * Return the power of a to n
 	 * 
 	 * @param a
 	 * @param n
-	 * @return
+	 * @return pow(a,n)
 	 */
-	public double power(double a,double n) {
+	public double power(double a, double n) {
 		String numberstring = Double.toString(n);
 		String[] numberArray = numberstring.split("\\.");
-		if(Integer.parseInt(numberArray[1]) > 0) {
-			return fractionPower(a, n); 
+		if (Integer.parseInt(numberArray[1]) > 0) {
+			return fractionPower(a, n);
 		}
-		if(n<0.0) {
-			a = 1.0/a;
-			n = n*-1;
-			return power(a,n);
-		}else if(n == 0.0) {
+		if (n < 0.0) {
+			a = 1.0 / a;
+			n = n * -1;
+			return power(a, n);
+		} else if (n == 0.0) {
 			return 1.0;
-		}else if((n%2)==0.0) {
-			a = a*a;
-			n = n/2;
-			return power(a,n);
-		}else {
-			n = (n-1)/2;
-			return a*power(a*a,n);
+		} else if ((n % 2) == 0.0) {
+			a = a * a;
+			n = n / 2;
+			return power(a, n);
+		} else {
+			n = (n - 1) / 2;
+			return a * power(a * a, n);
 		}
 	}
-	
+
 	/**
 	 * Calculate the power function for the decimal number
 	 * 
 	 * @param a
 	 * @param n
-	 * @return
+	 * @return pow(a,n)
 	 */
-	public double fractionPower(double a,double n) {
+	public double fractionPower(double a, double n) {
 		double answer = 0;
 		double logvalue = 0;
-		if(n==0)
+		if (n == 0) {
 			return 1;
-		if(a<0)
-			logvalue = ln(a*(-1));
-		else
-			logvalue = ln(a);
-		if(a==0 && n>0)
-			return answer;
-		for(int i=0;i<=125;i++) {
-			double numerator = power((n*logvalue),i);
-			double denominator = factorial(i);
-			answer = answer + (numerator/denominator);
 		}
-		if(a<0 && n%2!=0)
-			return answer*(-1);
-		else
+		if (a < 0) {
+			logvalue = ln(a * (-1));
+		} else {
+			logvalue = ln(a);
+		}
+		if (a == 0 && n > 0) {
 			return answer;
-		
+		}
+		for (int i = 0; i <= 125; i++) {
+			double numerator = power((n * logvalue), i);
+			double denominator = factorial(i);
+			answer = answer + (numerator / denominator);
+		}
+		if (a < 0 && n % 2 != 0) {
+			return answer * (-1);
+		} else {
+			return answer;
+		}
 	}
-	
+
 	/**
-	 * Return the log 
+	 * Return the log
 	 * 
 	 * @param n
-	 * @return
+	 * @return log n
 	 */
 	public double ln(double n) {
 		double answer = 0;
-		double base = (n-1)/(n+1);
-		for(int i=1;i<=125;i++) {
-			double exponent = 2*i-1;
-			answer += (1/exponent)*(power(base,exponent));
+		double base = (n - 1) / (n + 1);
+		for (int i = 1; i <= 125; i++) {
+			double exponent = 2 * i - 1;
+			answer += (1 / exponent) * (power(base, exponent));
 		}
-		return 2*answer;
+		return 2 * answer;
 	}
-	
-	
+
 	/**
 	 * tail recursive function for factorial
 	 * 
 	 * @param n
 	 * @param a
-	 * @return
+	 * @return n!
 	 */
-	public double factorialTailRecursive(double n,double a) {
-		if(n==0)
+	public double factorialTailRecursive(double n, double a) {
+		if (n == 0) {
 			return a;
-		return factorialTailRecursive(n-1,n*a);
+		}
+		return factorialTailRecursive(n - 1, n * a);
 	}
-	
+
 	/**
 	 * Return the factorial of number using the tail recursion
 	 * 
 	 * @param n
-	 * @return
+	 * @return n!
 	 */
 	public double factorial(double n) {
 		return factorialTailRecursive(n, 1);
 	}
-	
+
 	/**
-	 * This function returns true is the given number is Whole numbers(Z+) otherwise returns false
+	 * This function returns true is the given number is Whole numbers(Z+) otherwise
+	 * returns false
 	 * 
 	 * @param n
-	 * @return
+	 * @return true if the given number is whole number else false
 	 */
 	public boolean checkWholeNumber(double n) {
-		if(n%1==0)
+		if (n % 1 == 0) {
 			return true;
+		}
 		return false;
 	}
 }
--- src/test/Function6Test.java
+++ src/test/Function6Test.java
@@ -6,10 +6,8 @@ package test;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
-//import java.io.ByteArrayInputStream;
-//import java.io.ByteArrayOutputStream;
-//import java.io.FileInputStream;
-//import java.io.PrintStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,26 +21,28 @@ import main.Function6;
  *
  */
 class Function6Test {
-	
+
 	static Function6 fobject;
+
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		 fobject = new Function6();
+		fobject = new Function6();
 	}
-
-	
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
-		
+
 	}
-	
+
+	/**
+	 * Test the Beta Function for the x and y in Z+
+	 */
 	@Test
 	void testBetaFunction() {
 		assertEquals(fobject.betaFucntion(1, 1), 1);
@@ -51,47 +51,74 @@ class Function6Test {
 		assertNotNull(fobject.betaFucntion(500, 500));
 //		assertEquals(fobject.betaFucntion(1000, 7458), Double.NaN);
 	}
-	
-	
+
 	/**
 	 * Test Cases where Beta Function have decimal numbers
 	 */
 	@Test
 	void testBetaFunctionFraction() {
 		assertEquals(fobject.betaFucntion(1.5, 1.5), 0.28893183744773054);
-		assertEquals(fobject.betaFucntion(15.8, 20.8), 1.1488056591230658E-11);	
+		assertEquals(fobject.betaFucntion(15.8, 20.8), 1.1488056591230658E-11);
 	}
-	
-	
+
+	/**
+	 * Test the Best function where one or more inputs is zero
+	 */
 	@Test
 	void testBetaFunctionZero() {
-		assertThrows(StackOverflowError.class, () -> {fobject.betaFucntion(0, 0);});
-		assertThrows(StackOverflowError.class, () -> {fobject.betaFucntion(0, 5);});
-		assertThrows(StackOverflowError.class, () -> {fobject.betaFucntion(5, 0);});
+		assertThrows(StackOverflowError.class, () -> {
+			fobject.betaFucntion(0, 0);
+		});
+		assertThrows(StackOverflowError.class, () -> {
+			fobject.betaFucntion(0, 5);
+		});
+		assertThrows(StackOverflowError.class, () -> {
+			fobject.betaFucntion(5, 0);
+		});
 	}
-	
+
+	/**
+	 * Test the Best function where one or more inputs is negative
+	 */
 	@Test
 	void testBetaFunctionNegative() {
-		assertThrows(StackOverflowError.class, () -> {fobject.betaFucntion(0, -5);});
-		assertThrows(StackOverflowError.class, () -> {fobject.betaFucntion(-5, 5);});
-		assertThrows(StackOverflowError.class, () -> {fobject.betaFucntion(-5, -5);});
-		assertThrows(NumberFormatException.class, () -> {fobject.betaFucntion(-5.3, -5.6);});
+		assertThrows(StackOverflowError.class, () -> {
+			fobject.betaFucntion(0, -5);
+		});
+		assertThrows(StackOverflowError.class, () -> {
+			fobject.betaFucntion(-5, 5);
+		});
+		assertThrows(StackOverflowError.class, () -> {
+			fobject.betaFucntion(-5, -5);
+		});
+		assertThrows(NumberFormatException.class, () -> {
+			fobject.betaFucntion(-5.3, -5.6);
+		});
 	}
-	
+
+	/**
+	 * Test for the tail recursive factorial function
+	 */
 	@Test
 	void testFactorial() {
-		assertEquals(120,fobject.factorial(5));
-		assertEquals(1,fobject.factorial(0));
-		assertEquals(7.257415615308004E306,fobject.factorial(170));
+		assertEquals(120, fobject.factorial(5));
+		assertEquals(1, fobject.factorial(0));
+		assertEquals(7.257415615308004E306, fobject.factorial(170));
 	}
-	
+
+	/**
+	 * Test for the Log Function
+	 */
 	@Test
 	void testLn() {
-		assertEquals(6.589454748803522,fobject.ln(2345));
-		assertEquals(-6.791826429960522,fobject.ln(0));
+		assertEquals(6.589454748803522, fobject.ln(2345));
+		assertEquals(-6.791826429960522, fobject.ln(0));
 		assertEquals(6.347984312475613, fobject.ln(1000));
 	}
-	
+
+	/**
+	 * To check the given number is whole number or not
+	 */
 	@Test
 	void testCheckWholeNumber() {
 		assertFalse(fobject.checkWholeNumber(0.12));
@@ -99,7 +126,10 @@ class Function6Test {
 		assertTrue(fobject.checkWholeNumber(12.00));
 		assertTrue(fobject.checkWholeNumber(45));
 	}
-	
+
+	/**
+	 * Test for the power function
+	 */
 	@Test
 	void testPower() {
 		assertEquals(6.458728823355533E-5, fobject.power(5.6, -5.6));
@@ -111,7 +141,10 @@ class Function6Test {
 		assertEquals(1.0, fobject.power(144, 0.0));
 		assertEquals(1.0, fobject.power(0.0, 0.0));
 	}
-	
+
+	/**
+	 * Test for the Gamma Function for both Whole Number and Real Number
+	 */
 	@Test
 	void testGammaFunction() {
 		assertEquals(24.0, fobject.gammaFunction(5));
@@ -121,45 +154,56 @@ class Function6Test {
 		assertEquals(Double.POSITIVE_INFINITY, fobject.gammaFunction(204));
 		assertEquals(Double.POSITIVE_INFINITY, fobject.gammaFunction(546.2));
 	}
-	
-//	@Test
-//	void testMainFunction() {
-//		try {
-//			String userInput = String.format("9%s9%sy%ssdf%s-4%s-4%s-4%s0%s0%s-3%s0%s0%s1%s3%s0",
-//		            System.lineSeparator(),
-//		            System.lineSeparator());
-////			ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
-//		    System.setIn(new ByteArrayInputStream(userInput.getBytes()));
-//		    
-//		    String expected = "4.570592805886924E-6\r\n"
-//		    		+ "Error !\r\n"
-//		    		+ "Error !\r\n"
-//		    		+ "Error !\r\n"
-//		    		+ "Error !\r\n"
-//		    		+ "Error !\r\n"
+
+	/**
+	 * Test for the main function
+	 */
+	@Test
+	void testMainFunction() {
+		try {
+
+//		    String expected = "4.570592805886924E-6\n"
+//		    		+ "Error !\n"
+//		    		+ "Error !\n"
+//		    		+ "Error !\n"
+//		    		+ "Error !\n"
+//		    		+ "Error !\n"
 //		    		+ "0.3333333333333333";
 //		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
 //		    PrintStream printStream = new PrintStream(baos);
 //		    System.setOut(printStream);
 //		    
-//		    fobject.main(null);
-//		    
+			
+//			String userInput = "9" + System.getProperty("line.separator") + "9" + System.getProperty("line.separator")
+//			+ "y" + System.getProperty("line.separator") + "6" + System.getProperty("line.separator") + "-4"
+//			+ System.getProperty("line.separator") + "-4" + System.getProperty("line.separator") + "-4"
+//			+ System.getProperty("line.separator") + "0" + System.getProperty("line.separator") + "0"
+//			+ System.getProperty("line.separator") + "-3" + System.getProperty("line.separator") + "0"
+//			+ System.getProperty("line.separator") + "0" + System.getProperty("line.separator") + "1"
+//			+ System.getProperty("line.separator") + "3" + System.getProperty("line.separator") + "0"
+//			+ System.getProperty("line.separator");
+//			InputStream bais = new ByteArrayInputStream(userInput.getBytes());
+//			System.setIn(bais);
+//			
+//			fobject.main(null);
+
+			
+
 //		    String[] lines = baos.toString().split(System.lineSeparator());
 //		    String actual = lines[lines.length-1];
 //
 //		    // checkout output
 //		    assertEquals(expected,actual);
-//		}catch(Exception e) {
-//			
-//		}
-//	}
-	
+		} catch (Exception e) {
+
+		}
+	}
+
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-		System.gc();
 	}
 
 }
